ad6b9a31da83626cf4a97bc15ea06b7bea0e5e87
“Bojun <wanbojun@amazon.com>
2025-02-26 21:11:06 -0500

    Config change for demo using netlify

--- src/config/index.tsx
+++ src/config/index.tsx
@@ -1,36 +1,53 @@
-import { WagmiAdapter } from '@reown/appkit-adapter-wagmi'
-import { mainnet, arbitrum, solana, solanaDevnet, solanaTestnet, } from '@reown/appkit/networks'
-import type { AppKitNetwork } from '@reown/appkit/networks'
-import { SolanaAdapter } from '@reown/appkit-adapter-solana/react'
-import { PhantomWalletAdapter, SolflareWalletAdapter } from '@solana/wallet-adapter-wallets'
-
+import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
+import {
+  mainnet,
+  arbitrum,
+  solana,
+  solanaDevnet,
+  solanaTestnet,
+} from "@reown/appkit/networks";
+import type { AppKitNetwork } from "@reown/appkit/networks";
+import { SolanaAdapter } from "@reown/appkit-adapter-solana/react";
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
 
 // Get projectId from https://cloud.reown.com
-export const projectId = import.meta.env.VITE_PROJECT_ID || "b56e18d47c72ab683b10814fe9495694" // this is a public projectId only to use on localhost
+export const projectId =
+  import.meta.env.VITE_PROJECT_ID || "471f8b666303999a24c8ca965b2384ee";
+// "b56e18d47c72ab683b10814fe9495694"
+// this is a public projectId only to use on localhost
 
 if (!projectId) {
-  throw new Error('Project ID is not defined')
+  throw new Error("Project ID is not defined");
 }
 
 export const metadata = {
-    name: 'AppKit',
-    description: 'AppKit Example',
-    url: 'https://reown.com', // origin must match your domain & subdomain
-    icons: ['https://avatars.githubusercontent.com/u/179229932']
-  }
+  name: "AppKit",
+  description: "AppKit Example",
+  url: "https://delightful-tarsier-d8a8c0.netlify.app", // origin must match your domain & subdomain
+  icons: ["https://avatars.githubusercontent.com/u/179229932"],
+};
 
 // for custom networks visit -> https://docs.reown.com/appkit/react/core/custom-networks
-export const networks = [mainnet, arbitrum, solana, solanaDevnet, solanaTestnet] as [AppKitNetwork, ...AppKitNetwork[]]
+export const networks = [
+  mainnet,
+  arbitrum,
+  solana,
+  solanaDevnet,
+  solanaTestnet,
+] as [AppKitNetwork, ...AppKitNetwork[]];
 
 //Set up the Wagmi Adapter (Config)
 export const wagmiAdapter = new WagmiAdapter({
   projectId,
-  networks
-})
+  networks,
+});
 
 // Set up Solana Adapter
 export const solanaWeb3JsAdapter = new SolanaAdapter({
-  wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()]
-})
+  wallets: [new PhantomWalletAdapter(), new SolflareWalletAdapter()],
+});
 
-export const config = wagmiAdapter.wagmiConfig
+export const config = wagmiAdapter.wagmiConfig;
